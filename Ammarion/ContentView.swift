d3fade3f7860c61fda794525b06445106403d56a
mspanadgoo <mspanadgoo@me.com>
2021-07-05 10:39:34 +0430

    Initial Commit

---
+++ Ammarion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ammarion
+//
+//  Created by Mohammad Sadegh Panadgoo on 4/14/1400 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
